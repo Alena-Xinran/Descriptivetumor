7b7db5b11cd8349e31cdcc2d4258ed7c0ddc9b21
Alena-Xinran <113165964+Alena-Xinran@users.noreply.github.com>
2024-11-03 19:04:05 -0500

    Update hg.sh

--- STEP3.SegmentationModel/hg.sh
+++ STEP3.SegmentationModel/hg.sh
@@ -8,13 +8,14 @@ val_every=50
 workers=0
 organ=liver
 fold=0
+version=1
 
 # U-Net
 backbone=unet
 logdir="runs/$organ.fold$fold.$backbone"
 datafold_dir=cross_eval/"$organ"_aug_data_fold/
 dist=$((RANDOM % 99999 + 10000))
-python -W ignore main.py --model_name $backbone --cache_rate $cache_rate --dist-url=tcp://127.0.0.1:12565 --workers $workers --max_epochs 2000 --val_every $val_every --batch_size=$batch_size --save_checkpoint --distributed --noamp --organ_type $organ --organ_model $organ --tumor_type tumor --fold $fold --ddim_ts 50 --logdir=$logdir --healthy_data_root $healthy_datapath --data_root $datapath --datafold_dir $datafold_dir
+python -W ignore main.py --model_name $backbone --cache_rate $cache_rate --dist-url=tcp://127.0.0.1:12565 --workers $workers --max_epochs 2000 --val_every $val_every --batch_size=$batch_size --save_checkpoint --distributed --noamp --organ_type $organ --organ_model $organ --tumor_type tumor --fold $fold --ddim_ts 50 --logdir=$logdir --healthy_data_root $healthy_datapath --data_root $datapath --datafold_dir $datafold_dir --version $version
 
 # # nnU-Net
 # backbone=nnunet
